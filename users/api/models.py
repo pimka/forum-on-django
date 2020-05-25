d398433549878288d3c9bd237c9c8f073899cec6
pimka <zelenova.yulia@mail.ru>
2020-05-25 16:48:02 +0300

    test

--- users/api/models.py
+++ users/api/models.py
@@ -3,5 +3,6 @@ import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
     uuid = models.UUIDField(default=uuid.uuid4, editable=False)
